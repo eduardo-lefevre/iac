610002906cd2cbd71a584e91311ea2ed5aac0af5
elefevre66 <eduardo@lefevre.com>
2023-10-15 15:37:57 -0300

    Add SQ H2 DB datafiles and proceeding with TF SQ usage

--- terraform/sonarqube/imports.tf
+++ terraform/sonarqube/imports.tf
@@ -24,3 +24,25 @@
 #  id = "org.elefevre:iac/${var.org}/iac"
 #  to = sonarqube_github_binding.achievement-service-github
 #}
+
+# import {
+#   id = "AYsgFgdsjVxmbgG13ueS"
+#   to = sonarqube_group.team-users
+# }
+
+# import {
+#   id = "sonar-administrators[admin]"
+#   to = sonarqube_group_member.sonar-administrators_admin
+# }
+
+
+# import {
+#   id = "team-users[saml-user]"
+#   to = sonarqube_group_member.team-users_saml-user
+# }
+
+
+# import {
+#   id = "team-users[user]"
+#   to = sonarqube_group_member.team-users_user
+# }
